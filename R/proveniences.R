efec0dd98414d1be25adeee4c0f0d08fcaef0ef4
pg <palla.pg@gmail.com>
2017-09-06 19:02:17 +0200

    * create function filter_dataset to select province and municipality and change get_global_provinces * change get_provenience_by_nation

--- R/proveniences.R
+++ R/proveniences.R
@@ -1,17 +1,17 @@
 get_global_proveniences <- function(dataset, province_abbreviation, municipality_code){
-        if(!is.null(province_abbreviation)){
-          dataset <-  filter(dataset, provincia == province_abbreviation)
-          if (!is.null(municipality_code)){
-            if (substring(municipality_code, 1, 1) == "9"){
-              municipality_code = paste("0", municipality_code, sep = '')
-              print("*** municipality code modified")
-              print(municipality_code)
-            }
-            dataset <- dataset %>% filter(codicecomune == municipality_code)
-          }
-          
-        }
-        
+        # if(!is.null(province_abbreviation)){
+        #   dataset <-  filter(dataset, provincia == province_abbreviation)
+        #   if (!is.null(municipality_code)){
+        #     if (substring(municipality_code, 1, 1) == "9"){
+        #       municipality_code = paste("0", municipality_code, sep = '')
+        #       print("*** municipality code modified")
+        #       print(municipality_code)
+        #     }
+        #     dataset <- dataset %>% filter(codicecomune == municipality_code)
+        #   }
+        #   
+        # }
+        dataset <- filter_dataset(dataset, province_abbreviation, municipality_code)
         italians <- dataset %>% filter(grepl("^9", codicenazione)) %>% filter(!grepl("^9999", codicenazione)) %>% filter(anno_rif == 2016) 
         foreigners <- dataset %>% filter(!grepl("^9", codicenazione)) %>% filter(anno_rif == 2016)
         
@@ -24,7 +24,8 @@ get_global_proveniences <- function(dataset, province_abbreviation, municipality
         proveniences
 }
 
-get_provenience_by_nation <- function(dataset){
+get_provenience_by_nation <- function(dataset,province_abbreviation, municipality_code){
+        dataset <- filter_dataset(dataset, province_abbreviation, municipality_code)
         foreigners <- dataset %>% filter(!grepl("^9", codicenazione))
         proveniences <- aggregate(foreigners$tot_arrivi ~ foreigners$descrizione, FUN = sum)
         names(proveniences) <- c("nazione", "arrivi")
