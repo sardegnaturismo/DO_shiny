c36d7395c622e3cfb092c1f9e3e2d959b26d933e
pg <palla.pg@gmail.com>
2017-09-21 15:13:21 +0200

    * change the signature of the get_sex, get_accomodated_type and get_range to manage click events from maps and diagrams

--- R/profiling.R
+++ R/profiling.R
@@ -1,14 +1,14 @@
 require(dplyr)
-get_sex <- function(dataset, province_abbreviation, municipality_code){
-        dataset <- filter_dataset(dataset, province_abbreviation, municipality_code)
+get_sex <- function(dataset, province_abbreviation, municipality_code, prov_pie_event, nation_bar_ev){
+        dataset <- filter_dataset(dataset, province_abbreviation, municipality_code, prov_pie_event, NULL, nation_bar_ev)
         sex <- aggregate(dataset$tot_arrivi ~ dataset$sesso_str, FUN = sum)
         names(sex) <- c("sesso", "movimenti")
         sex <- sex %>% filter(grepl("[F|M]", sesso))
 
 }
 
-get_accomodated_type <- function(dataset, province_abbreviation, municipality_code){
-        dataset <- filter_dataset(dataset, province_abbreviation, municipality_code)
+get_accomodated_type <- function(dataset, province_abbreviation, municipality_code, prov_pie_event, profile_pie_event, nation_bar_ev){
+        dataset <- filter_dataset(dataset, province_abbreviation, municipality_code, prov_pie_event, profile_pie_event, nation_bar_ev)
         accomodated_type <- aggregate(dataset$tot_arrivi ~ dataset$tipoalloggiato_str, FUN = sum)
         names(accomodated_type) <- c("tipo_alloggiato", "arrivi")
         accomodated_type <- accomodated_type %>% filter(!(tipo_alloggiato == ''))
@@ -16,8 +16,8 @@ get_accomodated_type <- function(dataset, province_abbreviation, municipality_co
  
 }
 
-get_age_range <- function(dataset, province_abbreviation, municipality_code){
-        dataset <- filter_dataset(dataset, province_abbreviation, municipality_code)        
+get_age_range <- function(dataset, province_abbreviation, municipality_code, prov_pie_event, profile_pie_event, nation_bar_ev){
+        dataset <- filter_dataset(dataset, province_abbreviation, municipality_code, prov_pie_event, profile_pie_event, nation_bar_ev)        
         dd <- dataset  %>% 
                 filter(!(fasciaeta == "ND"))
         age_range <- aggregate(dd$tot_arrivi ~ dd$fasciaeta, FUN = sum)
