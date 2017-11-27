e33a9944fb2236e8299b0235bab92fcd07eb2c3d
Pg <palla.pg@gmail.com>
2017-11-27 15:05:07 +0100

    * change tryCatch blocks inside all profiling methods to manage exceptions

--- R/profiling.R
+++ R/profiling.R
@@ -3,9 +3,12 @@ get_sex <- function(dataset, province_abbreviation, municipality_code, prov_pie_
         dataset <- filter_dataset(dataset, province_abbreviation, municipality_code, prov_pie_event, NULL, nation_bar_ev, region_ev, NULL, lang_chosen)
         sex <- tryCatch({
                 aggregate(dataset$tot_arrivi ~ dataset$sesso_str, FUN = sum)},
-                finally = {data.frame(matrix(nrow = 1, ncol = 2))})
+                error = function(cond){
+                        message("Aggregate function does not have row to aggregate")
+                        sex = data.frame(matrix(nrow = 1, ncol = 2))
+                })
         names(sex) <- c("sesso", "movimenti")
-        sex <- sex %>% filter(grepl("[F|M]", sesso))
+        sex <- sex %>% filter(grepl("[F|M]", sesso))   
 
 }
 
@@ -13,10 +16,15 @@ get_accomodated_type <- function(dataset, province_abbreviation, municipality_co
         dataset <- filter_dataset(dataset, province_abbreviation, municipality_code, prov_pie_event, profile_pie_event, nation_bar_ev, region_bar_ev, NULL, lang_chosen)
         accomodated_type <- tryCatch({
                 aggregate(dataset$tot_arrivi ~ dataset$tipoalloggiato_str, FUN = sum)},
-                finally = {data.frame(matrix(nrow = 1, ncol = 2))})
+                error = function(cond){
+                        message("Aggregate function does not have row to aggregate")
+                        accomodated_type = data.frame(matrix(nrow = 1, ncol = 2))
+                        })
         names(accomodated_type) <- c("tipo_alloggiato", "arrivi")
         accomodated_type <- accomodated_type %>% filter(!(tipo_alloggiato == ''))
         accomodated_type = accomodated_type[order(accomodated_type$arrivi, decreasing = T), ]
+
+        
  
 }
 
@@ -26,7 +34,10 @@ get_age_range <- function(dataset, province_abbreviation, municipality_code, pro
                 filter(!(fasciaeta == "ND"))
         age_range <- tryCatch({
                 aggregate(dd$tot_arrivi ~ dd$fasciaeta, FUN = sum)},
-                finally = {data.frame(matrix(nrow = 1, ncol = 2))})                
+                error = function(cond){
+                        message("Aggregate function does not have row to aggregate")
+                        age_range = data.frame(matrix(nrow = 1, ncol = 2))                        
+        })                
         names(age_range) = c("fasciaeta", "arrivi")
         result <- age_range %>% mutate(fasciaeta = gsub("eta_", "", fasciaeta) %>% gsub("65\\+", ">65", .))
         result$fasciaeta = factor(x = result$fasciaeta, levels = result$fasciaeta)
